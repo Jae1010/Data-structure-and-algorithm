7e55b14972251c868636fb3d92a5318b545048db
Jae <45396620+workhard1010@users.noreply.github.com>
2020-10-11 16:17:42 +0800

    Update Vector.cpp

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -73,5 +73,81 @@ void Vector<T>::unsort(Rank lo, Rank hi) {
 //		swap(V[i - 1], V[rand() % i]);
 //}
 
+template <typename T> // 无序向量的顺序查找：返回最后一个元素e的位置；失败时，返回lo - 1
+Rank Vector<T>::find(T const& e, Rank lo, Rank hi) const {
+	//while ((lo < hi--) && (e != _elem[hi]));
+	while (lo < hi--)  //命中多个元素，将返回秩最大者
+	{
+		if (e == _elem[hi])
+			return hi;
+	}
+	return hi;
+}
 
+//插入元素
+template <typename T>
+Rank Vector<T>::insert(Rank r, T const& e) {
+	expand();
+	for (int i = _size; i > r; --i)
+		_elem[i] = _elem[i - 1];
+	_elem[r] = e;
+	return r;
+}
+
+//删除区间元素
+template <typename T>
+int Vector<T>::remove(Rank lo, Rank hi) {
+	if (hi == lo) return 0;
+	if (lo < 0 || hi > _size) return 0;
+	while (hi < _size)
+	{
+		_elem[lo++] = _elem[hi++];
+	}
+	_size = lo;
+	shrink();
+	return hi - lo;
+}
+
+
+//删除单元素
+template <typename T>
+T Vector<T>::remove(Rank r) {
+	T e = _elem[r];
+	remove(r, r + 1);
+	return e;
+}
+
+template <typename T> 
+int Vector<T>::deduplicate() { //删除无序向量中重复元素
+	int oldSize = _size;
+	Rank i = 1;
+	while (i < _size)
+	{
+		if (find(_elem[i], 0, i) < 0)
+			++i;
+		else
+			remove(i);
+	}
+	return oldSize - _size;
+}
+
+template <typename T>
+int Vector<T>::uniquify() {//删除有序向量中重复元素（双指针）
+	int oldSize = _size;
+	int slowIndex = 0;
+	for (int fastIndex = 0; fastIndex < _size; ++fastIndex)
+		if (_elem[fastIndex] != _elem[fastIndex + 1])
+			_elem[++slowIndex] = _elem[fastIndex + 1];
+	return oldSize - slowIndex - 1;
+}
+
+template <typename T>
+void Vector<T>::traverse(void (*visit) (T&)) {
+	for (int i = 0; i < _size; i++) visit(_elem[i]);
+}
+
+
+template <typename T> template < typename VST> //元素类型、操作器
+void Vector<T>::traverse(VST & visit) //利用函数对象机制的遍历
+{ for (int i = 0; i < _size; i++) visit(_elem[i]); }
 
