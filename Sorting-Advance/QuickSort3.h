077ab10e75ba24305db6f252b48d2468713666d4
Jae <45396620+workhard1010@users.noreply.github.com>
2020-11-17 11:19:31 +0800

    Create QuickSort3.h

---
+++ Sorting-Advance/QuickSort3.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include "InsertionSort.h"
+#include <ctime>
+
+
+template <typename T>
+void __quickSort3(T arr[], int l, int r) {
+
+	if (r - l <= 15) {
+		insertionSort(arr, l, r);
+		return;
+	}
+
+	//partition
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);
+	T v = arr[l];
+
+	int lt = l; //arr[l+1, lt] < v
+	int gt = r + 1; //arr[gt, r] > v
+	int i = l + 1;
+	while (i < gt) {
+		if (arr[i] < v) {
+			swap(arr[i], arr[lt + 1]);
+			lt++;
+			i++;
+		}
+		else if (arr[i] > v) {
+			swap(arr[i], arr[gt - 1]);
+			gt--;
+		}
+		else {
+			i++;
+		}
+	}
+	swap(arr[l], arr[lt]);
+	__quickSort3(arr, 0, lt - 1);
+	__quickSort3(arr, gt, r);
+}
+
+
+template <typename T>
+void quickSort3(T arr[], int n) {
+	srand(time(NULL));
+	__quickSort3(arr, 0, n - 1);
+}
